3aad65965a93492b7a4349f8770ffdc591966e04
Jessica Duell <jessica.duell@pendo.io>
2023-03-12 11:32:12 -0400

    fix tests

--- single_number/single_number.test.js
+++ single_number/single_number.test.js
@@ -4,6 +4,7 @@ describe('single_number', ()=>{
     it('returns the odd number out correctly', ()=>{
         expect(single_number([2, 2, 1])).toEqual(1);
         expect(single_number([4, 1, 2, 1, 2])).toEqual(4);
-        expect(single_number[2, 3, 5, 4, 5, 3, 4]).toEqual(2)
+        expect(single_number([2, 3, 5, 4, 5, 3, 4])).toEqual(2)
+
     })
 })
